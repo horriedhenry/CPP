3e58a47a2ae62820e202f51019f084d25778ba7b
henry <shivahenry33@gmail.com>
2022-08-28 17:16:38 +0530

    recursion

--- recursion/fibonacci.cpp
+++ recursion/fibonacci.cpp
@@ -1,10 +1,23 @@
+//nth fibonacci number
 #include<iostream>
 using namespace std;
 
 int fibonacci(int n) {
+    //base condition
+    if (n==0)
+    {
+        return 0;
+    }
+    if (n==1)
+    {
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main(void)
 {
+    int ans = fibonacci(4);
+    cout<<ans;
     return 0;
 }
--- recursion/rec.md
+++ recursion/rec.md
@@ -2,3 +2,7 @@
 when a function finishes exceuting it is removed from the stack and 
 the **flow of program is restored to where that function was called.**
 ```
+
+```text
+when you wrtie recursion in a formula it is called recurrence relation
+```
