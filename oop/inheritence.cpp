4092a3ed3cd54558c68f0f4d30e6378742661aa6
henry <shivahenry33@gmail.com>
2023-06-02 23:27:15 +0530

    feat : oop/Multi-level inheritence

--- oop/inheritence.cpp
+++ oop/inheritence.cpp
@@ -2,34 +2,15 @@
 #include <stdio.h>
 using namespace std;
 // source : [https://www.javatpoint.com/cpp-inheritance]
-// # Single Level Inheritance
+// # Multi-Level Inheritance
 
 /*
-NOTES Example 2(ChatGpt Response):
-The code you provided demonstrates the concept of inheritance in C++. In this
-case, class B is inheriting privately from class A using the private access
-specifier.
-
-When a class is inherited privately, the public and protected members of the
-base class become private members of the derived class. However, the private
-members of the base class are not accessible directly by the derived class.
-
-In the given code, the private members a and b of class A cannot be accessed
-directly by class B. However, class B can access the public member function
-mul() of class A because it has been inherited privately.
-
-The display_mul() function in class B invokes the mul() function from class A,
-which returns the multiplication of a and b. The result is then displayed using
-cout.
-
-So, class B cannot directly access the private members
-a and b of class A. It can only access them indirectly through the public member
-functions of class A, such as mul().
+NOTES :
 */
 class Animal {
 // Base Class
 public:
-    std::string Type = "Animal";
+    std::string AnimalType = "Animal";
     void eat() {
         cout<<"Eating..."<<endl;
     }
@@ -38,7 +19,6 @@ public:
     }
 };
 
-// Single level Inheritance
 class Dog : public Animal {
 // Derived class -> inheriting from class Animal
 // Dog class now has access to all the public methods, and class members
@@ -49,53 +29,34 @@ public:
         cout<<"Dog Barks"<<endl;
     }
     void type() {
-        cout<<Type<<endl;
+        cout<<AnimalType<<endl;
     }
 };
 
-// Example 2
-class A {
-private:
-    int a = 5,b = 4;
-    void printAB() { cout << a << ","<<b<<endl; }
-    // TODO :
-    // create a method void printAB() { cout << a << " "<<b<<endl; }
-    // and check where can it be used
+// Multi-Level Inheritance
+// -> When a class inherits from one class which is further inherited by other
+// class. ex : A,B,C are the classes then B inherits from A, and C inherits B 
+// which inherits A
+class BabyDog : public Dog {
 public:
-    int mul() {
-        return a * b;
+    void weep() {
+        cout<<"BabyDog Weeping..."<<endl;
     }
-    void access_Printab_classA() {
-        // TODO solution
-        printAB();
+    void crawl() {
+        cout<<"BabyDogcrawling"<<endl;
     }
-};
-
-class B : private A {
-// When you use private access specifier , then all the public and protected
-// members of class A become private members of class B , hence they can only 
-// be accessed within the class i.e(B).
-//
-// ***Imp : The private members of class A cannot be inherited by B even When
-// you use private access specifier , that means you cannot use the private
-// attributes of class A in B (ex. we cannnot use a,b vars in this class even 
-// privately.
-// And as we know we cannot access private members with objects.
-public:
-    void display_mul() {
-        cout<<"a * b = "<<mul()<<endl;
-    }
-    void access_Printab_WithClassB() {
-        // TODO solution
-        access_Printab_classA();
+    void bark() {
+        cout<<"BabyDog Barks"<<endl;
+        Dog::bark();
     }
 };
 
 int main (int argc, char *argv[]) {
-    B b;
-    b.display_mul();
-    cout<<endl;
-    cout<<"Values of a, b that are in class A = ";
-    b.access_Printab_WithClassB();
+    BabyDog baby;
+    baby.weep();
+    baby.crawl();
+    baby.bark();
+    baby.Dog::bark(); // `::` -> scope resolution operator
+    // `::` -> used to specify which version of function to be used
     return 0;
 }
