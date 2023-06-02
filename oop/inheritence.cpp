a36c760ce2c05b2446dd00fae1bbf66595bbee9a
henry <shivahenry33@gmail.com>
2023-06-02 20:20:04 +0530

    feat : oop/Single level Inheritence ex2,[NOTES]

--- oop/inheritence.cpp
+++ oop/inheritence.cpp
@@ -4,6 +4,28 @@ using namespace std;
 // source : [https://www.javatpoint.com/cpp-inheritance]
 // # Single Level Inheritance
 
+/*
+NOTES Example 2(ChatGpt Response):
+The code you provided demonstrates the concept of inheritance in C++. In this
+case, class B is inheriting privately from class A using the private access
+specifier.
+
+When a class is inherited privately, the public and protected members of the
+base class become private members of the derived class. However, the private
+members of the base class are not accessible directly by the derived class.
+
+In the given code, the private members a and b of class A cannot be accessed
+directly by class B. However, class B can access the public member function
+mul() of class A because it has been inherited privately.
+
+The display_mul() function in class B invokes the mul() function from class A,
+which returns the multiplication of a and b. The result is then displayed using
+cout.
+
+So, class B cannot directly access the private members
+a and b of class A. It can only access them indirectly through the public member
+functions of class A, such as mul().
+*/
 class Animal {
 // Base Class
 public:
@@ -31,11 +53,49 @@ public:
     }
 };
 
+// Example 2
+class A {
+private:
+    int a = 5,b = 4;
+    void printAB() { cout << a << ","<<b<<endl; }
+    // TODO :
+    // create a method void printAB() { cout << a << " "<<b<<endl; }
+    // and check where can it be used
+public:
+    int mul() {
+        return a * b;
+    }
+    void access_Printab_classA() {
+        // TODO solution
+        printAB();
+    }
+};
+
+class B : private A {
+// When you use private access specifier , then all the public and protected
+// members of class A become private members of class B , hence they can only 
+// be accessed within the class i.e(B).
+//
+// ***Imp : The private members of class A cannot be inherited by B even When
+// you use private access specifier , that means you cannot use the private
+// attributes of class A in B (ex. we cannnot use a,b vars in this class even 
+// privately.
+// And as we know we cannot access private members with objects.
+public:
+    void display_mul() {
+        cout<<"a * b = "<<mul()<<endl;
+    }
+    void access_Printab_WithClassB() {
+        // TODO solution
+        access_Printab_classA();
+    }
+};
+
 int main (int argc, char *argv[]) {
-    Dog d;
-    d.eat();
-    d.see();
-    d.bark();
-    d.type();
+    B b;
+    b.display_mul();
+    cout<<endl;
+    cout<<"Values of a, b that are in class A = ";
+    b.access_Printab_WithClassB();
     return 0;
 }
