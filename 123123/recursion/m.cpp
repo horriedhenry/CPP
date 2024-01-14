413ef6fd8adc434d9ff79276e5a450c5e1e8b7ca
horriedhenry <shivahenry33@gmail.com>
2024-01-15 00:05:34 +0530

    feat : recursion, fibonacci(non_recursive).

--- 123123/recursion/m.cpp
+++ 123123/recursion/m.cpp
@@ -25,8 +25,41 @@ int factorial(int n) {
     // finally prints 120.
 }
 
+void print_fibonacci(int count) {
+    int curr, prev, next;
+    std::cout << 0 << " " << 1 << " ";
+    curr = 1;
+    prev = 0;
+    for (int i = 0; i < count - 2; i++) {
+        // count - 2 because 0 and 1 are constant, and 0, 1 are already printed those
+        // so use -2 , if -2 is not used this loop prints 0 1 and count no.of 
+        // fibonacci numbers
+        next = curr + prev;
+        prev = curr;
+        curr = next;
+        std::cout << next << " ";
+    }
+    std::cout << std::endl;
+}
+
+void print_fibonacci_(int count) {
+    int first = 0, second = 1, next;
+    std::cout << first << " " << second << " ";
+    for (int i = 0; i < count - 2; i++) {
+        // count - 2 because 0 and 1 are constant, and 0, 1 are already printed those
+        // so use -2 , if -2 is not used this loop prints 0 1 and count no.of 
+        // fibonacci numbers
+        next = first + second;
+        first = second;
+        second = next;
+        std::cout << next << " ";
+    }
+    std::cout << std::endl;
+}
+
 int main (int argc, char *argv[]) {
     // recursion(1);
-    std::cout << factorial(5) << std::endl;
+    // std::cout << factorial(5) << std::endl;
+    print_fibonacci(10);
     return 0;
 }
